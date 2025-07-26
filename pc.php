3ac36326177ae6fcf56071a1bcb672f333ff915e
Quaff03 <138432251+Quaff03@users.noreply.github.com>
2025-07-26 13:07:44 +0800

    更新样式和结构，添加页面标题，替换Font Awesome版本，优化注销按钮布局

--- pc.php
+++ pc.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>工作站控制向导 - PC配置</title>
-    <link rel="stylesheet" href="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="index.css" rel="stylesheet">
 </head>
 <?php
